0910db27d25fe95bcb561771bc524f510b21f6ac
witcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-07-29 20:08:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,4 @@
 $(".panel").click(function(){
     $(".panel").removeClass("active");
     $(this).addClass("active");
-    $(".icon").removeClass("active");
 });
-
-$(".panel1").click(function() {
-    $(".icon1").addClass("active");
-})
-
-$(".panel2").click(function() {
-    $(".icon2").addClass("active");
-})
-
-$(".panel3").click(function() {
-    $(".icon3").addClass("active");
-})
-
-$(".panel4").click(function() {
-    $(".icon4").addClass("active");
-})
-
-$(".panel5").click(function() {
-    $(".icon5").addClass("active");
-    $(".panel5").animate({
-        animation: 'none'
-    })
-    $(".panel5").css("background", "url(images/vecna.png)")
-})
